daafa8577784c38fdb1d06c1c8043dc7723cebd5
djsjah <awesom-e4000@mail.ru>
2026-03-19 12:40:54 +0300

    refactor: RealtimeApi implemented

--- src/main/java/com/chat/infrastructure/realtime/RealtimeTokenController.java
+++ src/main/java/com/chat/infrastructure/realtime/RealtimeTokenController.java
@@ -1,11 +1,10 @@
 package com.chat.infrastructure.realtime;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chat.api.RealtimeApi;
 import com.chat.model.RealtimeTokenDTO;
 import com.chat.security.guard.MemberGuard;
 
@@ -13,16 +12,16 @@ import com.chat.security.guard.MemberGuard;
 @RequestMapping("/api/realtime/token")
 @MemberGuard
 @RequiredArgsConstructor
-public class RealtimeTokenController {
+public class RealtimeTokenController implements RealtimeApi {
     private final RealtimeService realtimeService;
 
-    @GetMapping("/connection")
+    @Override
     public RealtimeTokenDTO generateConnectionToken() {
         return new RealtimeTokenDTO(realtimeService.generateConnectionToken());
     }
 
-    @GetMapping("/subscription/room/{roomId}")
-    public RealtimeTokenDTO generateSubscriptionToken(@PathVariable Long roomId) {
+    @Override
+    public RealtimeTokenDTO generateSubscriptionToken(Long roomId) {
         return new RealtimeTokenDTO(realtimeService.generateSubscriptionToken(roomId));
     }
 }
